4f02a71f270b4b55bf9438ba79546c9f112743bd
Igor <i3101q@gmail.com>
2021-03-20 18:27:36 +0700

    Импорт интерфейса

---
+++ main.py
@@ -0,0 +1,24 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from ui import Ui_MainWindow
+
+
+# Наследуемся от виджета из PyQt5.QtWidgets и от класса с интерфейсом
+class MyWidget(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        # Вызываем метод для загрузки интерфейса из класса Ui_MainWindow,
+        # остальное без изменений
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.run)
+
+    def run(self):
+        self.label.setText("OK")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
